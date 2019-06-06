482e3071683339945f1196164db7d2ceab00a6c5
ferrellave <51388280+ferrellave@users.noreply.github.com>
2019-06-06 14:25:11 -0500

    Update admin.py

--- geography/admin.py
+++ geography/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
 
 from .models import *
-
-admin.site.register(Language)
-admin.site.register(Currency)
-admin.site.register(Address)
-admin.site.register(Location)
-admin.site.register(Community)
-admin.site.register(Zone)
-admin.site.register(City)
-admin.site.register(State)
-admin.site.register(Region)
-admin.site.register(Country)
-admin.site.register(Continent)
