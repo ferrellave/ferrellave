a9cd0f19aeb15391da1ef13fdc342bfb0d6a670f
ferrellave <51388280+ferrellave@users.noreply.github.com>
2019-06-06 14:55:56 -0500

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -7,11 +7,8 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'owner', 'create_at', 'update_at')
 
 admin.site.register(Article, ArticleAdmin)
-admin.site.register(Tag)
 admin.site.register(Category)
 admin.site.register(Section)
-admin.site.register(Trend)
 admin.site.register(Config)
 admin.site.register(Profile)
 admin.site.register(Page)
-admin.site.register(PageSection)
